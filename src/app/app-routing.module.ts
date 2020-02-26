0d919bacd6ca87391611d7013868adf8be25bdf5
KFA <kamil.falinski@gmail.com>
2020-02-26 22:12:16 +0100

    Navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {SignUpPageComponent} from './sign-up-page/sign-up-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'signup', component: SignUpPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule, MatButtonModule, MatIconModule, _MatMenuDirectivesModule, MatMenuModule} from '@angular/material';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutPageComponent,
+    SignUpPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    _MatMenuDirectivesModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
